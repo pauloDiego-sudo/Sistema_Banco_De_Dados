9984c587ffa7341b5a5efc4fd6d5e83006ac5b92
unknown <paulo.meneses3636@gmail.com>
2024-09-12 21:07:27 -0300

    Fix: adding CROS

--- code/app/main.py
+++ code/app/main.py
@@ -8,10 +8,21 @@ from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from dotenv import load_dotenv
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --------------------------------
 # Rotas para Autenticação
